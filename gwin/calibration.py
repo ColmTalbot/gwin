e0a2a09936ea81f118339a9e349c9e4a1d045463
Colm Talbot <colm.talbot@ligo.org>
2018-07-18 17:22:38 -0400

    make recalibration objects know the ifo name

--- gwin/calibration.py
+++ gwin/calibration.py
@@ -25,9 +25,9 @@ class Recalibrate(object):
 
     name = 'none'
 
-    def __init__(self):
+    def __init__(self, ifo_name):
+        self.ifo_name = ifo_name
         self.params = dict()
-        pass
 
     def apply_calibration(self, strain):
         """Apply calibration model
@@ -67,7 +67,7 @@ class Recalibrate(object):
         """
 
         self.params.update({key[len(prefix):]: params[key]
-                            for key in params if key[:len(prefix)] == prefix})
+                            for key in params if prefix in key and self.ifo_name in key})
 
         strain_adjusted = self.apply_calibration(strain)
 
@@ -94,8 +94,9 @@ class Recalibrate(object):
             An instance of the class.
         """
         all_params = dict(cp.items(section))
-        params = {key[3:]: all_params[key] for key in all_params if ifo in key}
+        params = {key[3:]: all_params[key] for key in all_params if ifo.lower() in key}
         model = params.pop('model')
+        params['ifo_name'] = ifo.lower()
 
         return all_models[model](**params)
 
@@ -104,7 +105,7 @@ class CubicSpline(Recalibrate):
 
     name = 'cubic_spline'
 
-    def __init__(self, minimum_frequency, maximum_frequency, n_points):
+    def __init__(self, minimum_frequency, maximum_frequency, n_points, ifo_name):
         """
         Cubic spline recalibration
 
@@ -122,7 +123,7 @@ class CubicSpline(Recalibrate):
         n_points: int
             number of spline points
         """
-        Recalibrate.__init__(self)
+        Recalibrate.__init__(self, ifo_name=ifo_name)
         minimum_frequency = float(minimum_frequency)
         maximum_frequency = float(maximum_frequency)
         n_points = int(n_points)
@@ -130,11 +131,13 @@ class CubicSpline(Recalibrate):
         self.spline_points = np.logspace(np.log(minimum_frequency), np.log(maximum_frequency), n_points)
 
     def apply_calibration(self, strain):
-        amplitude_parameters = [self.params['amplitude_{}'.format(ii)] for ii in range(self.n_points)]
+        amplitude_parameters = [self.params['amplitude_{}_{}'.format(self.ifo_name, ii)]
+                                for ii in range(self.n_points)]
         amplitude_spline = UnivariateSpline(self.spline_points, amplitude_parameters)
         delta_amplitude = amplitude_spline(strain.sample_frequencies.numpy())
 
-        phase_parameters = [self.params['phase_{}'.format(ii)] for ii in range(self.n_points)]
+        phase_parameters = [self.params['phase_{}_{}'.format(self.ifo_name, ii)]
+                            for ii in range(self.n_points)]
         phase_spline = UnivariateSpline(self.spline_points, phase_parameters)
         delta_phase = phase_spline(strain.sample_frequencies.numpy())
 
